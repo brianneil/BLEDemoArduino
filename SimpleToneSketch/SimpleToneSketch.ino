49e00fc9ae21f6fbd758d3f9f5a0151da6f58f0f
Brian Neil <brianneil@users.noreply.github.com>
2016-02-17 18:03:42 -0800

    Takes in messages using the Black Widow BLE system
    And will play tones when it hears a message

---
+++ SimpleToneSketch/SimpleToneSketch.ino
@@ -0,0 +1,32 @@
+//This will create a simple chirper that plays tones when told to by the iOS app.
+
+//Includes
+#include <SoftwareSerial.h>
+
+//Globals
+int Buzzer = 6;
+int LED = 13;
+unsigned int buzzTimer = 0;
+SoftwareSerial BLE_Shield(4,5);
+
+void setup() {
+  pinMode(Buzzer, OUTPUT);  //Sets the pin to be an output pin
+  pinMode(LED, OUTPUT);
+  BLE_Shield.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (BLE_Shield.available())   //Meaning there is data available for us to look at.
+  {
+    Serial.println(BLE_Shield.read());
+    buzzTimer = millis();
+    tone(Buzzer, 500);      //Turn the buzzer and the LED on
+    digitalWrite(LED, HIGH);
+  }
+  if ((millis() - buzzTimer) >= 1000) {    //After a second, turn everything off
+    noTone(Buzzer);
+    digitalWrite(LED, LOW);
+  }
+}
+
